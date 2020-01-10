c1f3e556d9dd04cc8aa4aa175b24427879e7aae4
dujingya <djy8788>
2020-01-10 15:35:13 +0800

    s

---
+++ js数据类型判断/index.js
@@ -0,0 +1,10 @@
+var type = function(data) {
+    var toString = Object.prototype.toString;
+    var dataType = data instanceof Element
+        ? 'element' // 为了统一DOM节点类型输出
+        : toString
+            .call(data)
+            .replace(/\[object\s(.+)\]/, ''$1')
+        .toLowerCase()
+    return dataType
+}
